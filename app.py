d34028aa04afc89dc46cf37e2741e6a265f8266e
Ethan <ejo2117@columbia.edu>
2024-06-20 17:27:10 -0400

    get table headers from wiki

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import mechanicalsoup
 import pandas as pd
 import sqlite3
 
+browser = mechanicalsoup.StatefulBrowser()
+browser.open("https://en.wikipedia.org/wiki/Comparison_of_Linux_distributions")
+
+# extract table headers
+th = browser.page.find_all("th", attrs={"class": "table-rh"})
+distribution = [value.text for value in th]
+print(distribution)
