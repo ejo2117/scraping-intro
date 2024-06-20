c0ed0077076f12ceb88453c711ccd67ee8cf3fb4
Ethan <ejo2117@columbia.edu>
2024-06-20 17:47:31 -0400

    organize a dictionary

--- app.py
+++ app.py
@@ -14,4 +14,24 @@ distribution = distribution[:98]
 td = browser.page.find_all("td")
 columns = [value.text.replace("\n", "") for value in td]
 columns = columns[6:1084]
-print(columns)
+
+column_names = ["Founder", 
+                "Maintainer", 
+                "Initial_Release_Year", 
+                "Current_Stable_Version", 
+                "Security_Updates", 
+                "Release_Date", 
+                "System_Distribution_Commitment", 
+                "Forked_From", 
+                "Target_Audience", 
+                "Cost", 
+                "Status"]
+
+dictionary = {"Distribution": distribution}
+
+#select every 11th item
+for idx, key in enumerate(column_names):
+    dictionary[key] = columns[idx:][::len(column_names)]
+
+df = pd.DataFrame(data = dictionary)
+print(df.tail())
