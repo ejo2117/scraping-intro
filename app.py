924b3fd0993de09473d38b27909707e5541a081f
Ethan <ejo2117@columbia.edu>
2024-06-21 13:18:42 -0400

    split artist and title columns

--- app.py
+++ app.py
@@ -5,28 +5,23 @@ import sqlite3
 browser = mechanicalsoup.StatefulBrowser()
 browser.open("https://kworb.net/spotify/country/global_weekly_totals.html")
 
-
-# thing = "Artist name - song name - remix"
-# nospace = [x.strip() for x in thing.split("-", 1)]
-# print(nospace)
-
-# extract table headers
-# th = browser.page.find_all("td", attrs={"class": "text mp"})
-# artistAndTitle = [value.text for value in th]
-# artistAndTitle = artistAndTitle[:98]
-# print(artistAndTitle)
-
-# # extract table data
+# extract table data
 td = browser.page.find_all("td")
 columns = [value.text for value in td]
-# columns = columns[6:1084]
-
-# f = open("test.txt", "a")
-# f.write(str(columns))
-# f.close()
 
 
-column_names = ["Artist_And_Title", 
+split_columns = []
+for idx, value in enumerate(columns):
+    if idx % 7 == 0:
+        [artist, title] = [x.strip() for x in value.split("-", 1)]
+        split_columns.append(artist)
+        split_columns.append(title)
+    else:
+        split_columns.append(value)
+
+
+column_names = ["Artist",
+                "Title", 
                 "Weeks", 
                 "Weeks_In_T10",
                 "Peak_Position",
@@ -34,25 +29,25 @@ column_names = ["Artist_And_Title",
                 "Peak_Streams", 
                 "Total_Streams"]
 
-# dictionary = {"Distribution": distribution} 
 dictionary = {}
+columns = split_columns
 
-# # select every 11th item
+# select every 8th item
 for idx, key in enumerate(column_names):
+    value = columns[idx:][::len(column_names)]
     dictionary[key] = columns[idx:][::len(column_names)]
-    # dictionary["Position"] = idx
 
 df = pd.DataFrame(data = dictionary)
 
 # print(df.head())
 
-# # insert data into a database
+# insert data into a database
 connection = sqlite3.connect("spotify_weekly.db")
 cursor = connection.cursor()
 
 cursor.execute("create table spotify ( " + ",".join(column_names) + ")")
 for i in range(len(df)):
-    cursor.execute("insert into spotify values (?,?,?,?,?,?,?)", df.iloc[i])
+    cursor.execute("insert into spotify values (?,?,?,?,?,?,?,?)", df.iloc[i])
 
 connection.commit()
 
